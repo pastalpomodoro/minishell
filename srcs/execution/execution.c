856cac939a7b0a0fa13fbea7271c48e6936e6e3f
Thomas Gastelu <tgastelu@bess-f2r5s12.clusters.42paris.fr>
2025-03-25 18:29:52 +0100

    fixed problem fils de pute de correcteur qui nous as mis faux pour un test de conard

--- srcs/execution/execution.c
+++ srcs/execution/execution.c
@@ -6,7 +6,7 @@
 /*   By: tgastelu <tgastelu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 12:51:45 by tgastelu          #+#    #+#             */
-/*   Updated: 2025/03/21 14:34:07 by rbaticle         ###   ########.fr       */
+/*   Updated: 2025/03/25 18:28:06 by tgastelu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void	exec(t_commande *cmd, t_commande *before, t_data *data, char **env)
 	{
 		save = dup(STDOUT_FILENO);
 		save_in = dup(STDIN_FILENO);
-		if (ft_strcmp(cmd->cmd[0], "exit"))
+		if (cmd->cmd && ft_strcmp(cmd->cmd[0], "exit"))
 			cmd->exit_code = dup2_our_cmd(cmd, cmd->next, before);
 		if (cmd->exit_code == 0)
 			if_statement(cmd, data);
@@ -70,7 +70,7 @@ int	fork_create(t_commande *cmd, t_data *data, char **env)
 		{
 			if (manage(cmd, before, data, env) == -1)
 				return (free_cmd(&init, NULL), -1);
-			if (before && ft_strcmp(before->cmd[0], "exit"))
+			if (before && before->cmd && ft_strcmp(before->cmd[0], "exit"))
 				close(before->pipe_fd[0]);
 			before = cmd;
 		}
